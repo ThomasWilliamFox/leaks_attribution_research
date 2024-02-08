6a9867b5ae8338c814aa36bc9d34d11aaa88d0dc
ThomasWilliamFox <thomas.fox@mail.utoronto.ca>
2024-02-08 18:26:07 -0500

    - Started replications - Raw Data added - Download instructions added

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -11,16 +11,20 @@
 #### Workspace setup ####
 library(opendatatoronto)
 library(tidyverse)
-# [...UPDATE THIS...]
+
 
 #### Download data ####
-# [...ADD CODE HERE TO DOWNLOAD...]
 
+# Raw Data for replication in excel format can be found at this URL:
+# https://dataverse.harvard.edu/dataset.xhtml?persistentId=doi:10.7910/DVN/SPLDTF
+#
+# After downloading, open with excel and select "no" when prompted to open as "read only"
+# File > Save As > "raw_data.xlsx" 
+# Save the file to > leaks_attribution_research/data/raw_data
 
+raw_data <- read_xlsx("data/raw_data/raw_data.xlsx")
 
-#### Save data ####
-# [...UPDATE THIS...]
-# change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
+#### Save data as csv ####
+write_csv(raw_data, "data/raw_data/raw_data.csv") 
 
          
--- scripts/99-replications.R
+++ scripts/99-replications.R
@@ -10,10 +10,57 @@
 
 #### Workspace setup ####
 library(tidyverse)
-# [...UPDATE THIS...]
+
 
 #### Load data ####
-# [...ADD CODE HERE...]
+raw_publication_data <- read_csv("data/raw_data/raw_data.csv", show_col_types = FALSE)
 
+
+#### Clean data ####
+
+# Isolate publication data
+publication_data <- raw_publication_data[c(119:685), c(0:21)]
+
+# Convert top row to column names
+# Uses stackoverflow query answer: https://stackoverflow.com/a/57531480
+names(publication_data) <- 
+  publication_data |>
+  slice(1) |>
+  unlist()
+publication_data <- publication_data |> slice(-1)
+
+
+# Make journal name key
+journal_names <- raw_publication_data[c(24:43), c(16,17)]
+
+journal_names <- 
+  journal_names |>
+  rename(name = ...16 , shortform = ...17)
+
+
+# Graph 1 
+
+# Select only number of cables cited
+cables_cited <-
+  publication_data |>
+  select(`NUM CABLES`) 
+
+# Convert "??" to -1 
+cables_cited$`NUM CABLES`[cables_cited$`NUM CABLES` == "??"] <- "-1"
+
+# Covert all entries to numbers
+cables_cited$`NUM CABLES` <- as.numeric(cables_cited$`NUM CABLES`)
+
+# Ommit all N/A articles
+cables_cited <- 
+  cables_cited |> na.omit(cables_cited)
+
+# Make tibble with 0 - max number of cables cited (16)
+graph1_data <- 
+  tibble( 
+    number_cited = c(0:max(cables_cited)),
+    )
+
+graph1_data
 
 
